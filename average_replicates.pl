9bd2cb5a10e6bac4d42354ed08946a982d2e181c
Yevhen Vainshtein <homeveg@gmail.com>
2017-01-13 16:25:24 +0100

    Revert "Revert "fix coordinates""
    This reverts commit faef9a5a729db526a2014b811acd4c8cab50e963.

--- average_replicates.pl
+++ average_replicates.pl
@@ -21,7 +21,7 @@ perl -w average_replicates.pl --dir=<path to working dir> --output=<path to resu
     --pattern | -p     occupancy profile file name extension template (default: occ.gz)
     --printData | -d   print all input occupancy columns to the output file
     --sum | -s         print column with sum of all occupancies for each nucleotide
-		
+	--list | -l        text file containing coma-separated list of all replicates (full path)		
     --gzip | -z        compress the output
     --help | -h        Help
     
@@ -105,11 +105,13 @@ my $printSum;
 
 my $needsHelp;
 my $useGZ;
+my $list_file;
 
 my $options_okay = &Getopt::Long::GetOptions(
 	'dir|i=s' => \$wd,
 	'output|out=s'   => \$output,
 	'pattern|p=s' => \$filename_pattern,
+	'list|l=s' => \$list_file,
 
 	'coordsCol|cC=s' => \$coordsCol,
 	'occupCol|oC=s' => \$occupCol,
@@ -137,33 +139,64 @@ else {
 }
 
 
+if($list_file) { print STDERR "loading replicates list file: $list_file\n"; }
+
 my $tm = localtime;
 print STDERR "-----------------------\n",
 join("-",$tm -> [3],1+ $tm -> [4],1900 + $tm -> [5])," ",
 join(":",$tm -> [2],$tm -> [1],$tm -> [0]),
 "\n-----------------------\n";
 
-#check if folder exists
+#load list of replicated experiments
+my (@names,@files,@dirs, @all_files);
 
-opendir(DIR, "$wd") or die $!;
-my @all_files = readdir(DIR);
-closedir(DIR);
-my (@names,@files);
+if ($list_file) {
+	open(LIST_FILE, "<$list_file") or die "can't read from file $list_file: $!";
+	my @lines=<LIST_FILE>;
+	close (LIST_FILE);
+	my $pattern="[\\t\\s,;]";
+	@all_files=split($pattern,join("",@lines));	
+} else {
+	opendir(DIR, "$wd") or die $!;
+	@all_files = readdir(DIR);
+	closedir(DIR);
+}
 
 foreach my $file (sort @all_files){
   if ($file =~ m/.*\.$filename_pattern$/){
 	push(@files, $file);
-	my $filename = basename($file,  "\.$filename_pattern");
-	push(@names, $filename);
+	my $file_name = basename($file,  "\.$filename_pattern");
+	my $dir_name = dirname($file,  "\.$filename_pattern");
+	push(@names, $file_name);
+	push(@dirs, $dir_name);
 	}
 }
 
 for (my $i=0; $i<=$#files; $i++) {
-	my $filename = $names[$i];
+	my $file_name = $names[$i];
 	my $file = $files[$i];
-	$NormFactors{$filename} = ReadFile("$wd/$file", $filename, $coordsCol, $occupCol, \%occupancy, @names);
+	my $dir = $dirs[$i];
+	$NormFactors{$file_name} = ReadFile("$dir/$file", $file_name, $coordsCol, $occupCol, \%occupancy, @names);
 
 }
+
+#
+#my (@names,@files);
+#
+#foreach my $file (sort @all_files){
+#  if ($file =~ m/.*\.$filename_pattern$/){
+#	push(@files, $file);
+#	my $filename = basename($file,  "\.$filename_pattern");
+#	push(@names, $filename);
+#	}
+#}
+#
+#for (my $i=0; $i<=$#files; $i++) {
+#	my $filename = $names[$i];
+#	my $file = $files[$i];
+#	$NormFactors{$filename} = ReadFile("$wd/$file", $filename, $coordsCol, $occupCol, \%occupancy, @names);
+#
+#}
 
 print STDERR "calculating StDev, Variance, Sum and average.\nResults will be saved to $output\n";
 
@@ -365,11 +398,11 @@ sub check_opts {
 			-message => "Error specifying options."
 		);
 	}
-	if ( ! -d $wd ) {
+	if ( ( ! -d $wd ) && ( ! $list_file ) ) {
 		pod2usage(
 			-exitval => 2,
 			-verbose => 1,
-			-message => "Cannot find input directory $wd: $!\n"
+			-message => "Cannot find either input directory $wd or list file $list_file: $!\n"
 		);
 	}
 	#if ( -e $outfile ) {
